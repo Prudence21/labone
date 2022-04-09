b28302b12dc8632a7aa35aa9d0532baa6e3f3099
noli Blessing <prettyprudy542@gmail.com>
2022-04-09 14:59:39 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace labone
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your name: ");
+            string myname=Console.ReadLine();
+            Console.WriteLine("Enter your track: ");
+            string mytrack=Console.ReadLine();
+            Console.WriteLine("Hello {0}, I am in {1} of Wella Schools ",myname,mytrack );
+                
+            Console.WriteLine("I'm learning Backend development because i want to know about building the framework of websites and webapps. ");
+        }
+    }
+}
